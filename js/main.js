739b15dc151272a9680fa8f078e9344c5fd3eedd
Sebastian <dev_sebasfc@outlook.es>
2022-07-31 22:34:54 -0500

    Creado productos con javascript auto

--- js/main.js
+++ js/main.js
@@ -43,3 +43,53 @@ function OpenOrder(){
 Carrito.addEventListener('click',OpenOrder);
 navEmail.addEventListener('click',openmenu);
 menuHamburgesa.addEventListener('click',openMenuMobile);
+
+const Products = [];
+Products.push({nombre:'Bike',costo:200,imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'});
+Products.push({nombre:'Gorra',costo:30,imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'});
+Products.push({nombre:'Bike',costo:200,imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'});
+
+function arrayProduct(arr){
+for (product of arr){
+
+    const divCardcontainer = document.querySelector('.card-container');
+    
+    const divProductcontainer = document.createElement('div');
+    divProductcontainer.classList.add('product-container');
+
+    const imagencard = document.createElement('img');
+    imagencard.setAttribute('src',product.imagen);
+
+    const divProductInfo = document.createElement('div');
+    divProductInfo.classList.add('product-info');
+
+    const divcontainerElement = document.createElement('div');
+
+    const p_nombre = document.createElement('p');
+    p_nombre.innerText = product.nombre;
+
+    const p_costo = document.createElement('p');
+    p_costo.innerText = '$' + product.costo;
+
+    const divcontainerelement2 = document.createElement('div');
+
+    const figure = document.createElement('figure');
+
+    const imagecard = document.createElement('img');
+    imagecard.setAttribute('src','https://raw.githubusercontent.com/platzi/curso-frontend-developer-practico/4774124de4c723d46d6a22da4c93c03f7be9e7bd/icons/bt_add_to_cart.svg');
+
+    figure.appendChild(imagecard);
+    divcontainerelement2.appendChild(figure);
+
+    divcontainerElement.appendChild(p_nombre);
+    divcontainerElement.appendChild(p_costo);
+    divProductInfo.appendChild(divcontainerElement);
+    divProductInfo.appendChild(divcontainerelement2);
+
+    divProductcontainer.appendChild(imagencard);
+    divProductcontainer.appendChild(divProductInfo);
+
+    divCardcontainer.appendChild(divProductcontainer);
+}
+}
+arrayProduct(Products);
